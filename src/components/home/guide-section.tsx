bc4a4334c5e0d2e6f3028385ac77a1b6cac176cf
Mohit Singh <msxcodes@msxcodes.kali>
2024-05-16 10:26:22 +0530

    step buttons for mobile

--- src/components/home/guide-section.tsx
+++ src/components/home/guide-section.tsx
@@ -8,16 +8,41 @@ import "./home.css";
 import Feature1 from "../../../public/home/Feature-1.png";
 import Feature2 from "../../../public/home/Feature-2.png";
 import Feature3 from "../../../public/home/Feature-3.png";
-import { useEffect, useState } from "react";
+import LeftArrow from "../../../public/home/left-arrow.svg";
+import RightArrow from "../../../public/home/right-arrow.svg";
+import { useEffect, useRef, useState } from "react";
 
 interface StepsSectionProps {}
 
 export const GuideSection: React.FC<StepsSectionProps> = ({}) => {
+    const boxRef = useRef();
+    const [isBtn, setIsBtn] = useState<boolean>(false);
+
+    const rightArrowHandler = () => {
+        const box: any = boxRef.current;
+        box != "undefined" && (box.scrollLeft += box.clientWidth);
+        // box.scrollLeft >= 0 && box.scrollLeft <= 450
+        //     ? setBtn(true)
+        //     : setBtn(false),
+        // console.log(box.scrollLeft)
+    };
+
+    const leftArrowHandler = () => {
+        const box: any = boxRef.current;
+        box != "undefined" && (box.scrollLeft -= box.clientWidth);
+        // box.scrollLeft >= 450 && box.scrollLeft >= 500
+        //     ? setBtn(true)
+        //     : setBtn(false),
+        // console.log(box.scrollLeft);
+    };
     return (
         <>
-            <div className="relative mb-5 flex items-center justify-center md:mb-0">
-                <div className="custom-scrollbar overflow-y- mx-2 flex w-[100%] snap-x snap-mandatory items-center overflow-x-scroll scroll-smooth md:mb-10 md:block md:h-[80vh] md:w-[100%] md:snap-y md:flex-col md:justify-around md:gap-6 md:overflow-x-auto md:overflow-y-scroll">
-                    <div className="snap-center  ">
+            <div className="relative flex flex-col items-center justify-center md:mb-0 md:flex-row">
+                <div
+                    ref={boxRef}
+                    className="custom-scrollbar main-container mx-2 flex w-[100%] snap-x snap-mandatory items-start overflow-x-hidden overflow-y-hidden  scroll-smooth md:mb-10 md:block md:h-[80vh] md:w-[100%] md:snap-y md:snap-mandatory md:flex-col md:items-center md:justify-center md:gap-6 md:overflow-x-auto md:overflow-y-auto"
+                >
+                    <div className="snap-center snap-always">
                         <GuideStep
                             stepNum={"1 :"}
                             stepName={"Comment"}
@@ -31,7 +56,7 @@ export const GuideSection: React.FC<StepsSectionProps> = ({}) => {
                             }
                         />
                     </div>
-                    <div className="snap-center">
+                    <div className="snap-center snap-always">
                         <GuideStep
                             stepNum={"2 :"}
                             stepName={"Automated Reply & DM"}
@@ -45,7 +70,7 @@ export const GuideSection: React.FC<StepsSectionProps> = ({}) => {
                             }
                         />
                     </div>
-                    <div className="snap-center">
+                    <div className="snap-center snap-always">
                         <GuideStep
                             stepNum={"3 :"}
                             stepName={"Sales Conversion"}
@@ -59,6 +84,20 @@ export const GuideSection: React.FC<StepsSectionProps> = ({}) => {
                             }
                         />
                     </div>
+                </div>
+            </div>
+            <div className="mx-8 grid grid-cols-2 gap-2  rounded-full bg-[#FDEDF1] px-2 py-2 md:hidden">
+                <div
+                    onClick={leftArrowHandler}
+                    className={`flex items-center justify-center rounded-full bg-[${"#e11c93"}]`}
+                >
+                    <Image src={LeftArrow} className="py-4" alt="" />
+                </div>
+                <div
+                    onClick={rightArrowHandler}
+                    className={`flex w-full items-center justify-center rounded-full bg-[${"#e11c93"}]`}
+                >
+                    <Image src={RightArrow} alt="" className="py-4" />
                 </div>
             </div>
         </>
